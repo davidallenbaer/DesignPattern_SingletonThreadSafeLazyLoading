b376b12d1cb49d1b80a1d2ac709386f8b3f3376f
davidallenbaer <davidallenbaer@gmail.com>
2021-05-18 23:16:30 -0400

    Add project files.

---
+++ DesignPattern_SingletonThreadSafeLazyLoading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_SingletonThreadSafeLazyLoading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => PrintTeacherDetails(),
+                () => PrintStudentdetails()
+                );
+            Console.ReadLine();
+        }
+        private static void PrintTeacherDetails()
+        {
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From Teacher");
+        }
+        private static void PrintStudentdetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
